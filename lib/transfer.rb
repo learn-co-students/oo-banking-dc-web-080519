95d63b6aab4614940c4f2aea707caaa37f4f4778
Pete Hanner <petehanner@gmail.com>
2019-08-08 14:36:52 -0400

    OO transfer

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,14 +9,15 @@ class Transfer
     @status = "pending"
   end
 
+
   def valid?
-    self.sender.valid? && self.receiver.valid?
+    self.sender.valid? && self.receiver.valid? && self.sender.balance >= self.amount
   end
 
   def execute_transaction
     if !self.valid?
       self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     elsif self.status == "pending"
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
@@ -24,4 +25,14 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    # ensure status is complete
+    if self.status == "complete"
+      # if so, exchange balance
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      # change status to reversed
+      self.status = "reversed"
+    end
+  end
 end
